652e883084e5ded89b52659f769d89f69f2fd2aa
Coralistone <mcoralistone@gmail.com>
2022-05-05 00:47:29 +0200

    push

---
+++ Master_chef_console/MODEL/RESTAURATION/PersonneCuisine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_chef_console.MODEL.RESTAURATION
+{
+    public class PersonnelCuisine
+    {
+
+        public PersonnelCuisine(String Nom, String Prenom)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+            IsBusy = false;
+        }
+
+        private String Nom;
+
+        private String Prenom;
+
+        private Boolean IsBusy;
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public String GetNom()
+        {
+            return this.Nom;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public String GetPrenom()
+        {
+            return this.Prenom;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public Boolean GetIsBusy()
+        {
+            return this.IsBusy;
+        }
+
+        /// <summary>
+        /// @param Boolean
+        /// </summary>
+
+        public void SetIsBusy(Boolean State)
+        {
+            this.IsBusy = State;
+        }
+
+    }
+}
