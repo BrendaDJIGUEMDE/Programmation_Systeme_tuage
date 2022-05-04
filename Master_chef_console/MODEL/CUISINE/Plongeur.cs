33b6f2b245d6e5571efd8a4cc1cdfc940128fc86
Cedric-Law <cedrictoe01@gmail.com>
2022-05-05 00:26:44 +0200

    suite

--- Master_chef_console/MODEL/CUISINE/Plongeur.cs
+++ Master_chef_console/MODEL/CUISINE/Plongeur.cs
@@ -1,12 +1,41 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Master_chef_console.MODEL.CUISINE
-{
-    internal class Plongeur
+public class Plongeur : PersonnelCuisine {
+
+    private static Plongeur Instance = null;
+
+    /// <summary>
+    /// Private constructor of the Singleton
+    /// </summary>
+    private Plongeur(String nom = "ERREUR", String prenom = "ERREUR") : base(nom, prenom) {
+    }
+
+
+    /// <summary>
+    /// Creation and return of the singleton with args
+    /// </summary>
+    public static Plongeur GetInstance(String nom, String prenom)
     {
+        if (Instance == null)
+        {
+            Plongeur.Instance = new Plongeur(nom, prenom);
+        }
+        return Plongeur.Instance;
+    }
+
+    /// <summary>
+    /// Creation and return of the singleton without args
+    /// </summary>
+    public static Plongeur GetInstance()
+    {
+        if (Instance == null)
+        {
+            Plongeur.Instance = new Plongeur();
+        }
+        return Plongeur.Instance;
     }
 }
