62c475fc9e60f4b81bbf38370237791c157b72bd
NathanaelDefo <74901693+NathanaelDefo@users.noreply.github.com>
2022-05-05 20:39:39 +0200

    Modif

--- MasterCuisine/Program.cs
+++ MasterCuisine/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MasterCuisine
 {
     internal class Program
     {
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            byte[] bytes = new Byte[1024];
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.7");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8651);
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine("connection success !!    "); Socket handler = listener.Accept();
+                    data = null;
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Text received : {0}", data);
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
         static void Main(string[] args)
         {
+            //Serveur
+            Console.WriteLine("Hello0000000000000000000000000000000")
+            StartListening();
+            Machines MachineUn = new M(achines("Four", 35);
         }
     }
 }
