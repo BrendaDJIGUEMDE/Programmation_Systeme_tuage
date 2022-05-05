25139d995ed0c27c52ea1ff72168cc5e876bbf74
Brenda <74040397+BrendaDJIGUEMDE@users.noreply.github.com>
2022-05-05 03:24:42 +0200

    add winform
    create and custom windows

--- Master_chef_Winform/Form1.cs
+++ Master_chef_Winform/Form1.cs
@@ -16,5 +16,25 @@ namespace Master_chef_Winform
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
